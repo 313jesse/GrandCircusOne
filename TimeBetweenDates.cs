cc36a521e0d7ff819d419a89a5c4bfec43bcdd09
Jesse <35883129+313jesse@users.noreply.github.com>
2018-06-30 14:12:41 -0400

    Add files via upload

---
+++ TimeBetweenDates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TimeBetweenDates
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            DateTime firstDate;
+            DateTime secondDate;
+
+            Console.WriteLine("Enter the first Date in dd/mm/yyyy format, then press ENTER.");
+            firstDate = Convert.ToDateTime(Console.ReadLine()).Date;
+
+            Console.WriteLine("Enter the second Date in dd/mm/yyyy format, then press ENTER.");
+            secondDate = Convert.ToDateTime(Console.ReadLine()).Date;
+
+            var difference = secondDate.Subtract(firstDate);
+
+            Console.WriteLine("The difference is " + difference.Days + " days.");
+            Console.WriteLine("The difference is " + difference.Days*24 + " hours.");
+            Console.WriteLine("The difference is " + difference.Days*24*60 + " Minutes.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
